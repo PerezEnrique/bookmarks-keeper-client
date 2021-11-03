a9f8c60737f950f2b5133a50b3a313c4e0dce2e7
Enrique Perez <28enriqueperez@gmail.com>
2021-11-03 06:57:56 -0400

    feat: Initial commit. Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+/** @type {import('webpack').Configuration} */
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	devServer: {
+		compress: true,
+		historyApiFallback: true,
+	},
+	devtool: "eval-source-map",
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.(ico)$/,
+				type: "assets/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: "./src/public/index.html",
+			filename: "./index.html",
+			favicon: "./src/public/favicon.ico",
+		}),
+	],
+};
