42ed005e8d5fffcf777ebd6d9e1407ec2d02d405
Enrique Perez <28enriqueperez@gmail.com>
2021-11-14 08:09:16 -0400

    feat: Implement joi validation on Edit bookmark form

--- src/components/EditBookmarkDialog.jsx
+++ src/components/EditBookmarkDialog.jsx
@@ -1,28 +1,45 @@
 import React, { useContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import {
+	Alert,
+	Box,
+	Button,
+	Chip,
+	Dialog,
+	DialogTitle,
+	Stack,
+	TextField,
+} from "@mui/material";
 import UserContext from "../contexts/UserContext";
-import { Box, Button, Chip, Dialog, DialogTitle, Stack, TextField } from "@mui/material";
+import useJoiValidation from "../hooks/useJoiValidation";
+import { editBookmark as editBookmarkSchema } from "../utils/validation-schemas/bookmarks-validation-schemas";
 
 export default function EditBookmarkDialog({
 	currentData: { _id, url: currentUrl, name: currentName, tags: currentTags },
 	open,
 	handleClose,
 }) {
-	const { editBookmark } = useContext(UserContext);
+	const { editBookmark, error } = useContext(UserContext);
 
 	const [url, setUrl] = useState(currentUrl);
 	const [name, setName] = useState(currentName);
 	const [tags, setTags] = useState(currentTags);
 	const [newTag, setNewTag] = useState("");
+	const [errors, setErrors] = useState(null);
 
 	//to add the new tag when user press enter
-	const handleKeyUp = (e) => {
-		if (e.code == "Enter") {
-			const tagsArray = [...tags];
-			tagsArray.push(newTag);
-			setTags(tagsArray);
-			setNewTag("");
+	const handleAddTag = (e) => {
+		if (newTag.length > 50) {
+			setErrors({
+				...errors,
+				tags: "tag length must be less or equal to 50 characters long",
+			});
+			return;
 		}
+		const tagsArray = [...tags];
+		tagsArray.push(newTag);
+		setTags(tagsArray);
+		setNewTag("");
 	};
 
 	const handleTagDelete = (tag) => {
@@ -34,42 +51,64 @@ export default function EditBookmarkDialog({
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
+		const validationErrors = useJoiValidation(editBookmarkSchema, { url, name, tags });
+		if (validationErrors) {
+			setErrors(validationErrors);
+			return;
+		}
+
 		await editBookmark(_id, { url, name, tags });
-		handleClose();
 	};
 
 	return (
 		<Dialog open={open} onClose={handleClose}>
 			<Box sx={{ px: 4 }}>
 				<DialogTitle>Edit bookmark</DialogTitle>
+				{error && (
+					<Alert severity="error" sx={{ mb: 1 }}>
+						{error}
+					</Alert>
+				)}
 				<Box
 					component="form"
 					onSubmit={handleSubmit}
 					sx={{
+						mb: 3,
 						display: "flex",
 						flexDirection: "column",
 						justifyContent: "space-around",
 						alignItems: "center",
-						"& > :not(style)": { my: 1 },
+						"& > :not(style)": { my: 1, minWidth: "450px" },
 					}}
 				>
 					<TextField
 						label="url"
 						value={url}
 						onChange={(e) => setUrl(e.target.value)}
+						error={errors && errors.url && errors.url.length > 0}
+						helperText={errors && errors.url}
 					></TextField>
 					<TextField
 						label="name"
 						value={name}
 						onChange={(e) => setName(e.target.value)}
+						error={errors && errors.name && errors.name.length > 0}
+						helperText={errors && errors.name}
 					></TextField>
-					<TextField
-						label="tags"
-						value={newTag}
-						onChange={(e) => setNewTag(e.target.value)}
-						onKeyUp={handleKeyUp}
-						helperText="Add a tag and press enter"
-					></TextField>
+					<Stack direction="row" alignItems="center">
+						<TextField
+							sx={{ mr: 1 }}
+							label="tags"
+							value={newTag}
+							onChange={(e) => setNewTag(e.target.value)}
+							helperText="Add a tag and press enter"
+							error={errors && errors.tags && errors.tags.length > 0}
+							helperText={errors && errors.tags}
+						></TextField>
+						<Button variant="contained" onClick={handleAddTag}>
+							Add tag
+						</Button>
+					</Stack>
 					<Stack direction="row" spacing={1}>
 						{tags.map((tag) => (
 							<Chip
